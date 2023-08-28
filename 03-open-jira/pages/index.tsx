c6b08649963faa7f50b6e52d3ec82498b2be839d
Viviana Perez <vperez@kindersystems.com>
2023-08-28 18:47:57 -0500

    Lesson 03: feat add main design

--- 03-open-jira/pages/index.tsx
+++ 03-open-jira/pages/index.tsx
@@ -1,14 +1,49 @@
-import { Layout } from "@/components/layouts";
-import { Typography } from "@mui/material";
 import { NextPage } from "next";
+
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+
+import { Layout } from "@/components/layouts";
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <Layout>
-        <Typography variant="h1" color={"primary"}>
-          Hello World
-        </Typography>
+      <Layout title="Home - OpenJira">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <Card
+              sx={{
+                height: "calc(100vh - 100px)",
+              }}
+            >
+              <CardHeader title="Pending" />
+
+              <CardContent>
+                {/* Agregar una nueva tarea */}
+                {/* Listado de entradas */}
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Card
+              sx={{
+                height: "calc(100vh - 100px)",
+              }}
+            >
+              <CardHeader title="In Progress" />
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Card
+              sx={{
+                height: "calc(100vh - 100px)",
+              }}
+            >
+              <CardHeader title="Completed" />
+            </Card>
+          </Grid>
+        </Grid>
       </Layout>
     </>
   );
