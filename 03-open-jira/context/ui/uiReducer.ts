54d6d252a3dcf29a7f716f7640d9d3d9cb2ae304
Viviana Perez <vperez@kindersystems.com>
2023-08-28 17:50:29 -0500

    Lesson 03: feat: add reducer

--- 03-open-jira/context/ui/uiReducer.ts
+++ 03-open-jira/context/ui/uiReducer.ts
@@ -1,1 +1,23 @@
-export const uiReducer = () => {};
+import { UIState } from ".";
+
+type UIActionType =
+  | { type: "UI - Open Sidebar" }
+  | { type: "UI - Close Sidebar" };
+
+export const uiReducer = (state: UIState, action: UIActionType) => {
+  switch (action.type) {
+    case "UI - Open Sidebar":
+      return {
+        ...state,
+        sideMenuOpen: true,
+      };
+    case "UI - Close Sidebar":
+      return {
+        ...state,
+        sideMenuOpen: false,
+      };
+
+    default:
+      return state;
+  }
+};
