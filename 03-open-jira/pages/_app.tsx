6e9be8b889d624c6da2a6dcfd0e41269743b39eb
VivianaS14 <vasorianp@gmail.com>
2023-09-13 10:46:20 -0500

    Lesson 03: add uuid

--- 03-open-jira/pages/_app.tsx
+++ 03-open-jira/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
-
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
-
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { darkTheme, lightTheme } from "@/themes";
+
 import { UIProvider } from "@/context/ui";
+import { EntriesProvider } from "@/context/entries";
+
+import { darkTheme, lightTheme } from "@/themes";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -16,13 +17,15 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UIProvider>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <main className={roboto.className}>
-          <Component {...pageProps} />
-        </main>
-      </ThemeProvider>
-    </UIProvider>
+    <EntriesProvider>
+      <UIProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <main className={roboto.className}>
+            <Component {...pageProps} />
+          </main>
+        </ThemeProvider>
+      </UIProvider>
+    </EntriesProvider>
   );
 }
