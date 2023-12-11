48831f14ed124559bc9d563afdb0f1a451c4b450
VivianaS14 <vasorianp@gmail.com>
2023-12-11 11:52:29 -0500

    fix: middleware for entries by id

--- 03-open-jira/middleware.ts
+++ 03-open-jira/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/api/entries")) {
-    const id = request.nextUrl.pathname.replace("/api/entries", "");
+    const id = request.nextUrl.pathname.replace("/api/entries/", "");
     const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
 
     if (!checkMongoIDRegExp.test(id)) {
