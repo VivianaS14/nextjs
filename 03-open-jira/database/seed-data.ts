9baab1fd32b9ef6911a18f42156d64ed56b58376
VivianaS14 <vasorianp@gmail.com>
2023-12-11 11:56:49 -0500

    feat: seed data to database

--- 03-open-jira/database/seed-data.ts
+++ 03-open-jira/database/seed-data.ts
@@ -1,3 +1,5 @@
+import { Columns } from "@/interfaces";
+
 interface SeedEntry {
   description: string;
   status: string;
@@ -9,6 +11,51 @@ export const seedData: SeedEntry[] = [
     description:
       "Your task should look like this. If you want state to change, try drag the note and drop in other column.",
     createdAt: Date.now(),
-    status: "pending",
+    status: "Pending",
   },
 ];
+
+export const seedColumns = {
+  pending: {
+    columnId: 0,
+    title: "Pending",
+    entriesIds: [],
+  },
+  "in-progress": {
+    columnId: 1,
+    title: "In-progress",
+    entriesIds: [],
+  },
+  finished: {
+    columnId: 2,
+    title: "Finished",
+    entriesIds: [],
+  },
+};
+
+export const seedJira = {
+  entries: [
+    {
+      description: "A test",
+      createdAt: Date.now(),
+      status: "pending",
+    },
+  ],
+  columns: {
+    pending: {
+      id: 0,
+      title: "Pending",
+      entriesIds: [],
+    },
+    "in-progress": {
+      id: 1,
+      title: "In-progress",
+      entriesIds: [],
+    },
+    finished: {
+      id: 2,
+      title: "Finished",
+      entriesIds: [],
+    },
+  },
+};
